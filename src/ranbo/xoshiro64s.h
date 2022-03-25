727f15f9ca080914a523d5e5126c0ff5ce798e65
Mikkel Højlund Larsen <mikkelhojlundgrontmij@gmail.com>
2022-03-25 13:23:52 +0100

    optimized the xoshiro64s algorithm

--- src/ranbo/xoshiro64s.h
+++ src/ranbo/xoshiro64s.h
@@ -10,11 +10,14 @@
 #endif
 
 #define MULT_CONSTANT 0x9E3779BB
+#define MIX_CONSTANT_1 0x9e3779b97f4a7c15uLL
+#define MIX_CONSTANT_2 0xbf58476d1ce4e5b9uLL
+#define MIX_CONSTANT_3 0x94d049bb133111ebuLL
 
 struct ranbo_xoshiro64s
 {
-    uint64_t seed = 2203318231040;
-    uint32_t seed_parts[2] = {513U, 8192U};
+    uint64_t seed = 1;
+    uint32_t seed_parts[2] = {3793791033U, 2065550767U};
 };
 
 uint32_t ranbo_detail_xoshiro64s_rotate(uint32_t number, int bits_to_rotate)
@@ -22,25 +25,37 @@ uint32_t ranbo_detail_xoshiro64s_rotate(uint32_t number, int bits_to_rotate)
     return (number << bits_to_rotate) | (number >> (32 - bits_to_rotate));
 }
 
+uint64_t ranbo_detail_xoshiro64s_split_and_mix_seed(uint64_t* seed)
+{
+    uint64_t mix = (*seed += MIX_CONSTANT_1);
+    mix = (mix ^ (mix >> 30)) * MIX_CONSTANT_2;
+    mix = (mix ^ (mix >> 27)) * MIX_CONSTANT_3;
+    return mix ^ (mix >> 31);
+}
+
 // Do not pass 0 to this function please.
 void ranbo_xoshiro64s_set_seed(struct ranbo_xoshiro64s* generator,
                                uint64_t seed)
 {
     generator->seed = seed;
-    generator->seed_parts[0] = (uint32_t)((seed & 0xFFFFFFFF00000000LL) >> 32);
-    generator->seed_parts[1] = (uint32_t)(seed & 0xFFFFFFFFLL);
+    uint64_t temp_seed = 0;
+    temp_seed = ranbo_detail_xoshiro64s_split_and_mix_seed(&temp_seed);
+    generator->seed_parts[0] =
+        (uint32_t)((temp_seed & 0xFFFFFFFF00000000LL) >> 32);
+    generator->seed_parts[1] = (uint32_t)(temp_seed & 0xFFFFFFFFLL);
 }
 
 uint32_t ranbo_xoshiro64s_generate(struct ranbo_xoshiro64s* generator)
 {
-    const uint32_t s0 = generator->seed_parts[0];
     uint32_t s1 = generator->seed_parts[1];
-    uint32_t result = s0 * MULT_CONSTANT;
+    uint32_t result = generator->seed_parts[0] * MULT_CONSTANT;
 
-    s1 ^= s0;
+    s1 ^= generator->seed_parts[0];
     generator->seed_parts[0] =
-        ranbo_detail_xoshiro64s_rotate(s0, 26) ^ s1 ^ (s1 << 9);
-    generator->seed_parts[1] = ranbo_detail_xoshiro64s_rotate(s1, 13);
+        ranbo_detail_xoshiro64s_rotate(generator->seed_parts[0], 26) ^
+        generator->seed_parts[1] ^ (generator->seed_parts[1] << 9);
+    generator->seed_parts[1] =
+        ranbo_detail_xoshiro64s_rotate(generator->seed_parts[1], 13);
     generator->seed =
         ((uint64_t)generator->seed_parts[0]) << 32 | generator->seed_parts[1];
     return result;
--- test/src/test_xoshiro64s.cpp
+++ test/src/test_xoshiro64s.cpp
@@ -6,24 +6,19 @@ TEST(test_xoshiro64s, api)
 {
     struct ranbo_xoshiro64s generator;
 
-    EXPECT_EQ(generator.seed, 2203318231040U);
-    EXPECT_EQ(generator.seed_parts[0], 513U);
-    EXPECT_EQ(generator.seed_parts[1], 8192U);
-
-    ranbo_xoshiro64s_set_seed(&generator, 1U);
     EXPECT_EQ(generator.seed, 1U);
-    EXPECT_EQ(generator.seed_parts[0], 0U);
-    EXPECT_EQ(generator.seed_parts[1], 1U);
+    EXPECT_EQ(generator.seed_parts[0], 3793791033U);
+    EXPECT_EQ(generator.seed_parts[1], 2065550767U);
 
     uint32_t random_number = ranbo_xoshiro64s_generate(&generator);
-    EXPECT_EQ(random_number, 0U);
-    EXPECT_EQ(generator.seed, 2203318231040U);
+    EXPECT_EQ(random_number, 4020032163U);
+    EXPECT_EQ(generator.seed, 12037577613245804387U);
 
     random_number = ranbo_xoshiro64s_generate(&generator);
-    EXPECT_EQ(random_number, 220917691U);
+    EXPECT_EQ(random_number, 2576321781U);
 
     ranbo_xoshiro64s_set_seed(&generator, 2203318231040U);
     EXPECT_EQ(generator.seed, 2203318231040U);
-    EXPECT_EQ(generator.seed_parts[0], 513U);
-    EXPECT_EQ(generator.seed_parts[1], 8192U);
+    EXPECT_EQ(generator.seed_parts[0], 3793791033U);
+    EXPECT_EQ(generator.seed_parts[1], 2065550767U);
 }
