d3e2be246c030d565a5b0ff1899330f0173758a9
Mikkel Højlund Larsen <mikkelhojlundgrontmij@gmail.com>
2022-04-01 12:28:13 +0200

    fixed tests

--- test/src/test_xoshiro64s.cpp
+++ test/src/test_xoshiro64s.cpp
@@ -6,8 +6,8 @@ TEST(test_xoshiro64s, api)
 {
     struct ranbo_xoshiro64s generator;
 
-    EXPECT_EQ(generator.seed_parts[0], 3793791033U);
-    EXPECT_EQ(generator.seed_parts[1], 2065550767U);
+    EXPECT_EQ(generator.seed[0], 3793791033U);
+    EXPECT_EQ(generator.seed[1], 2065550767U);
 
     uint32_t random_number = ranbo_xoshiro64s_generate(&generator);
     EXPECT_EQ(random_number, 4020032163U);
@@ -17,6 +17,6 @@ TEST(test_xoshiro64s, api)
 
     ranbo_xoshiro64s_set_seed(&generator, 2203318231040U);
 
-    EXPECT_EQ(generator.seed_parts[0], 389342166U);
-    EXPECT_EQ(generator.seed_parts[1], 2540433894U);
+    EXPECT_EQ(generator.seed[0], 389342166U);
+    EXPECT_EQ(generator.seed[1], 2540433894U);
 }
